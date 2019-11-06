1d5edecc82d93f5b19dfea476783b41b33d63244
Madelyn Deshield <madelynwallop@gmail.com>
2019-11-06 04:10:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,14 @@ end
 def take_a_number (katz_deli, name)
     katz_deli.push "name"
     puts "Welcome, #{name}. You are number #{1} in line."
-    katz_deli.length
+    
 end
+   
+def now_serving(katz_deli)
+  if katz_deli ==0 
+    puts "There is nobody waiting in line!"
+   
+   
+   
+   
+   
