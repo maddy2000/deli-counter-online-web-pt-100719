da733183cc64e506d7a21f1c94f96528f04b0edc
Madelyn Deshield <madelynwallop@gmail.com>
2019-11-06 04:20:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,8 +15,8 @@ end
 end
 
 def take_a_number (katz_deli, name)
-    katz_deli.push "Ada"
-    puts "Welcome, #{name}. You are number #{1} in line."
+    katz_deli.push name
+    puts "Welcome, #{name}. You are number #{katz_deli} in line."
     
 end
    
