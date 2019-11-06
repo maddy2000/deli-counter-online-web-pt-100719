48d0ffb1b8811b03fb34714bd77f384f7686ba2a
Madelyn Deshield <madelynwallop@gmail.com>
2019-11-06 01:41:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+require 'pry'
+def line(katz_deli)
+  line = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number (katz_deli, name)
+  counter = 0 
+  if counter 0
+  puts "Welcome, #{name}. You are number #{1} in line."
+  
+end
+end
